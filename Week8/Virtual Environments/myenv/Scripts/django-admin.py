e0932930420a531c1d1fc519c247e9dbe3cf000d
estharossa <danik.encore@gmail.com>
2020-03-18 14:36:10 +0600

    added tasks, django project, venv

---
+++ Week8/Virtual Environments/myenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Daniyar\Desktop\4 semester\WebDev\Lab8\Virtual Environments\myenv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
